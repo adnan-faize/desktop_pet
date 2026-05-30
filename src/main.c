a3c62a9c00649019bf64608eaf2051546603c5ed
Adnan Faize <adnanfaize@gmail.com>
2026-05-30 10:07:02 +0200

    added base main and meson.build

---
+++ src/main.c
@@ -0,0 +1,13 @@
+/**
+ * @file main.c
+ *
+ * @brief ...
+ *
+ * @author Adnan Faize <adnanfaize@gmail.com>
+ */
+
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+    return EXIT_SUCCESS;
+}
